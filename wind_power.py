7058b00607b2f00e02c1911c3ecf81c3a4102393
Buse Yağmur Metiner <84142650+buseyagmurmetiner@users.noreply.github.com>
2023-01-13 02:29:37 +0300

    Add files via upload

--- wind_power.py
+++ wind_power.py
@@ -9,14 +9,14 @@ from keras.layers import Flatten
 from keras.layers import LSTM
 from math import sqrt
 
-dataset=pd.read.csv('T1.cvs')
+dataset=pd.read.csv('istanbul_clean.cvs')
 dataset.head()
-start_date=datetime(2018,1,1)
-period=10
+start_date=datetime(2022,1,1)
+period=60
 
 def spend_missing_data(dataset,missing_index):
     index_num=missing_index+0.5
-    yest_index=(missing_index + 1)-(24*6)
+    yest_index=(missing_index + 1)-(24*1)
     data=pd.DataFrame(dict(dataset.iloc[yest_index]),index=[index_num])
     
     index_date=datetime.strptime(dataset['Date/Time'].values[yest_index],'%d %m %Y %H %M')+ timedelta(days=+1)
@@ -32,20 +32,21 @@ for i in range (len(dataset)):
         break
     dataDate=datetime.strptime(dataset['Date/Time'].values[i],'%d %m %Y %H :%M')
     otherDate != datetime.strptime(dataset['Date/Time'].values[i+1],'%d %m %Y %H:%M')
-    if otherDate !=(dataDate+timedelta(minutes=10)):
+    if otherDate !=(dataDate+timedelta(minutes=60)):
         dataset=append_missing_data(dataset,i)
-    dataset.to_csv('T1_clean.csv')
+    dataset.to_csv('istanbul_clean.csv')
     #spliting dataset
+
 from numpy import array_split,array,split,asarray
 
-dataset=pd.read_csv('T1_clean.csv')
+dataset=pd.read_csv('istanbul_clean.csv')
 dataset.head()
 #split_type is used a hour value (12 or 24)
 def split_dataset(data,split_type !=24):
     if(split_type!= 12 and split_type !=24):
         raise Exception('split_count value should be 12 or 24')
         
-    train,test=data[1:43201],data[43201:50977]
+    train,test=data[1:7446],data[7446:8760]
     
     #split as 12 or 24 hour datas
     train=array(array_split(train,len(train)/(split_type*6)))
@@ -59,8 +60,8 @@ def show_scores(score,scores):
     s_scores=','.join(['%.1f' % s for s in scores])
     print('[%.3f] %s' % (score,s_scores))
 
-    #dataset=pd.read_csv('T1_clean.csv',header=0,infer_datetime_format=True,parse_dates=['Date/Time'])
-dataset=pd.read_csv('Dataset-1_for_Active_Power_Prediction.csv',header=0)
+    #dataset=pd.read_csv('istanbul_clean.csv',header=0,infer_datetime_format=True,parse_dates=['Date/Time'])
+dataset=pd.read_csv('Dataset-1_istanbul_res.csv',header=0)
 #split into train and test
 train,test=split_dataset(dataset.values)
 n_input=7
@@ -129,10 +130,10 @@ score=sqrt(s/(actual.shape[0]*actual.shape[1]))
 #summarize scores
 show_scores(score,scores)
 #plot scores
-start_date=datetime(2018,1,1)
+start_date=datetime(2022,1,1)
 columns=list()
 for i in range(72):
-    columns.append(start_date + timedelta(minutes=10*i))
+    columns.append(start_date + timedelta(minutes=60*i))
 pyplot.plot(columns,scores,marker='o',label='lstm')   
 pyplot.show()
 
